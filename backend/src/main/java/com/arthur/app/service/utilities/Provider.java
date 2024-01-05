1b1191b23a1ef83e978eeae0587c339246352e8e
Arthur Riechert <arthur@cybrco.ai>
2024-01-05 01:27:25 -0500

    Added enum to represent LLM API providers

---
+++ backend/src/main/java/com/arthur/app/service/utilities/Provider.java
@@ -0,0 +1,13 @@
+/**
+ * A basic enumeration of all supported AI service providers.
+ *
+ * NOTE:
+ * - Support for other models will be available in the future.
+ *
+ * @author Arthur Riechert
+ * @version 1.0.0
+ */
+public enum Provider {
+    OPENAI,
+    ANTHROPIC;
+}
