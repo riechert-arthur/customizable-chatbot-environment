be31e13ae874afb7c7539aa5f045d6eb4ee09a83
Arthur Riechert <arthur@cybrco.ai>
2024-01-09 22:34:13 -0500

    Added package

--- backend/src/main/java/com/arthur/app/service/utilities/Restful.java
+++ backend/src/main/java/com/arthur/app/service/utilities/Restful.java
@@ -1,9 +1,12 @@
+package com.arthur.service.utilies;
+
 /**
  * A class to be implemented by all things that involve API calls.
  *
  * @author Arthur Riechert
  * @version 1.0.0
  */
+
 public interface Restful {
 
     /** The API key needed to access the service. */
