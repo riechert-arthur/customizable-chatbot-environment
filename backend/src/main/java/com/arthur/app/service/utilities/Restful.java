f04f4f423f434a6cc4f45fb82831c5fbf5b6ba8b
Arthur Riechert <arthur@cybrco.ai>
2024-01-09 22:29:58 -0500

    Added interface to define static variables of classes that call APIs

---
+++ backend/src/main/java/com/arthur/app/service/utilities/Restful.java
@@ -0,0 +1,11 @@
+/**
+ * A class to be implemented by all things that involve API calls.
+ *
+ * @author Arthur Riechert
+ * @version 1.0.0
+ */
+public interface Restful {
+
+    /** The API key needed to access the service. */
+    private String API_KEY;
+}
